225cf466953b6ab1c4bbdd8c8725a0b8737da238
Yura Bublyk <yurabublik703@gmail.com>
2024-02-18 23:33:23 +0200

    first edition burger menu

---
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -0,0 +1,72 @@
+import '../Header/header.scss';
+import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
+import { IconLogo } from '../IconLogo';
+
+type Props = {
+  setCloseBurger: (state: boolean) => void
+};
+
+export const BurgerMenu: React.FC<Props> = ({
+  setCloseBurger
+}) => (
+  <aside id='burgerMenu' className='burger__menu'>
+    <div className="burger__top">
+      <div className='burger__logo'>
+        <IconLogo />
+      </div>
+
+      <a
+        onClick={() => {
+          setCloseBurger(false);
+        }}
+        href="#/"
+        className="logo_link burger__close"
+      >
+        <img
+          src="./logos/close.svg"
+          alt="layoutLogo"
+        />
+      </a>
+    </div>
+
+    <div className='burger__main'>
+      <ul className="burger-nav_list">
+        <li className="burger-list_item">
+          <NavLink
+            to="#/"
+            className={({ isActive }) =>
+              classNames('burger-nav_link', { 'burger-is-active': isActive })
+            }>
+              Home
+          </NavLink>
+        </li>
+        <li className="burger-list_item">
+          <NavLink to="#/" className="burger-nav_link">
+              Phones
+          </NavLink>
+        </li>
+        <li className="burger-list_item">
+          <NavLink to="#/" className="burger-nav_link">
+              Tablets
+          </NavLink>
+        </li>
+        <li className="burger-list_item">
+          <NavLink to="#/" className="burger-nav_link">
+              Accessories
+          </NavLink>
+        </li>
+      </ul>
+
+      <div className='burger__buttons'>
+        <a href="#/" className="burger__button">
+          <img src="./logos/favourites.svg" alt="logoFavourite" />
+        </a>
+
+        <a href="#/" className="burger__button">
+          <img src="./logos/shopping-bag.svg" alt="logoShoppingBag" />
+        </a>
+      </div>
+    </div>
+  </aside>
+);
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,51 +4,78 @@ import { NavLink } from 'react-router-dom';
 import '../../constants/constants.scss';
 import './header.scss';
 
+import { useState } from 'react';
 import { IconLogo } from '../IconLogo';
+import { BurgerMenu } from '../BurgerMenu';
 
-export const Header = () => (
-  <header className="header">
-    <nav className="nav">
-      <div className="nav_menu_start">
-        <IconLogo />
+export const Header = () => {
+  const [openBurger, setOpenBurger] = useState(false);
 
-        <ul className="nav_list">
-          <li className="list_item">
-            <NavLink
-              to="#/"
-              className={({ isActive }) =>
-                classNames('nav_link', { ' is-active': isActive })
-              }>
-              Home
-            </NavLink>
-          </li>
-          <li className="list_item">
-            <NavLink to="#/" className="nav_link">
-              Phones
-            </NavLink>
-          </li>
-          <li className="list_item">
-            <NavLink to="#/" className="nav_link">
-              Tablets
-            </NavLink>
-          </li>
-          <li className="list_item">
-            <NavLink to="#/" className="nav_link">
-              Accessories
-            </NavLink>
-          </li>
-        </ul>
-      </div>
+  return (
+    (
+      <>
+        <header className="header">
+          <nav className="nav">
+            <div className="nav_menu_start">
+              <IconLogo />
 
-      <div className="header__buttons-block">
-        <a href="#/" className="header__button header__button-link">
-          <img src="./logos/favourites.svg" alt="logoFavourite" />
-        </a>
+              <ul className="nav_list">
+                <li className="list_item">
+                  <NavLink
+                    to="#/"
+                    className={({ isActive }) =>
+                      classNames('nav_link', { ' is-active': isActive })
+                    }>
+                  Home
+                  </NavLink>
+                </li>
+                <li className="list_item">
+                  <NavLink to="#/" className="nav_link">
+                  Phones
+                  </NavLink>
+                </li>
+                <li className="list_item">
+                  <NavLink to="#/" className="nav_link">
+                  Tablets
+                  </NavLink>
+                </li>
+                <li className="list_item">
+                  <NavLink to="#/" className="nav_link">
+                  Accessories
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
 
-        <a href="#/" className="header__button header__button-link">
-          <img src="./logos/shopping-bag.svg" alt="logoShoppingBag" />
-        </a>
-      </div>
-    </nav>
-  </header>
-);
+            <div className="header__buttons-block">
+              <a href="#/" className="header__button">
+                <img src="./logos/favourites.svg" alt="logoFavourite" />
+              </a>
+
+              <a href="#/" className="header__button">
+                <img src="./logos/shopping-bag.svg" alt="logoShoppingBag" />
+              </a>
+            </div>
+
+            <div className='burger'>
+              <a
+                onClick={() => {
+                  setOpenBurger(true);
+                }}
+                href="#/"
+                className="header__burger-menu"
+              >
+                <img src="./logos/burger-menu.svg" alt="logoShoppingBag" />
+              </a>
+            </div>
+          </nav>
+        </header>
+
+        {openBurger
+        && <BurgerMenu
+          setCloseBurger={setOpenBurger}
+        />}
+      </>
+    )
+  );
+};
