1df1203391a5efa474ea8a73d4ef80d84379ade4
Oleksandr Boiko <iccc97@gmail.com>
2024-02-19 11:06:01 +0200

    add layout without styles for `CartPage`

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,12 +1,33 @@
 import { BackButton } from '../../components/BackButton';
 import { CartItem } from '../../components/CartItem';
-import '../../styles/blocks/container.scss';
+
+import './cartPage.scss';
+import '../../styles/blocks/button.scss';
 
 export const CartPage = () => {
   return (
     <div className="cart">
-      <BackButton />
-      
+      <div className="cart__back-button">
+        <BackButton />
+      </div>
+
+      <h1 className="cart__title h1">
+        Cart
+      </h1>
+
+      <article className="total">
+        <div className="total__info">
+          <div className="total__price">$1337</div>
+          <div className="total__count-items">Total for 3 items</div>
+        </div>
+
+        <span className="total__line" />
+
+        <button type="button" className="button">
+          Checkout
+        </button>
+      </article>
+
       <CartItem />
     </div>
   );
