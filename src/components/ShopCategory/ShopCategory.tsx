30816d4d44a14e3634d1912c5c7a80aa9b69c7d5
Vlad Gerula <vlad.gerula@gmail.com>
2024-02-19 10:16:58 +0200

    add with media adaptive, but without photos

--- src/components/ShopCategory/ShopCategory.tsx
+++ src/components/ShopCategory/ShopCategory.tsx
@@ -7,11 +7,14 @@ export const ShopCategory:React.FC = () => {
       <h2 className="section-title h2">Shop by category</h2>
       <div className="shop__by-category">
         <article className="product shop__product">
-          <img
-            src="../../img/category-phones.png"
-            alt="categoty-phone"
-            className="product__photo product__photo--phones"
-          />
+          <div className="product__photo-container">
+            <img
+              src="../../img/category-phones.png"
+              alt="categoty-phone"
+              className="product__photo product__photo--phones"
+            />
+          </div>
+
           <h3 className="product__title">
             Mobile phones
           </h3>
