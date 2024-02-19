cce0d988f999e76907670906bd1e666f3f0a6a1e
Nikita Krasun <heysleare.ls@gmail.com>
2024-02-19 14:55:22 +0200

    card layout

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,18 +1,19 @@
 import { Product } from '../enums/Product';
+import { Phone } from '../types/Phone';
 import { request } from '../utils/fetchClient';
 
-export const getAccessries = () => {
+export const accessries = () => {
   return request(Product.Accessories);
 };
 
-export const getPhones = () => {
-  return request(Product.Phones);
+export const phones = () => {
+  return request<Phone[]>(Product.Phones);
 };
 
-export const getTablets = () => {
+export const tablets = () => {
   return request(Product.Tablets);
 };
 
-export const getProducts = () => {
+export const products = () => {
   return request(Product.Products);
 };
--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,14 +1,32 @@
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { Pagination } from '../../components/Pagination';
+import * as getGoods from '../../api/products';
+import { Phone } from '../../types/Phone';
+import { CardLayout } from '../../components/CardLayout/CardLayout';
 
 export const CatalogPage = () => {
   const { currentPageNumber } = useParams();
+  const [phones, setPhones] = useState<Phone[]>([]);
+  const [amountOfPages, setAmountOfPages] = useState(1);
+
+  useEffect(() => {
+    getGoods.phones().then(data => {
+      setPhones(data);
+    });
+
+  }, []);
+
+  useEffect(() => {
+    setAmountOfPages( Math.ceil(phones.length / 16));
+  }, []);
 
   return (
     <>
+      <CardLayout phones={phones} />
       <div className="wrapper">
         <Pagination
-          amountOfPages={5}
+          amountOfPages={amountOfPages}
           currentPageIndex={+(currentPageNumber as string) - 1}
         />
       </div>
