3d85f00f98bd36b126664f7335d6ced8ac327d56
Oleksandr Boiko <iccc97@gmail.com>
2024-02-19 10:38:35 +0200

    add `BackButton`

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,9 +1,12 @@
+import { BackButton } from '../../components/BackButton';
 import { CartItem } from '../../components/CartItem';
 import '../../styles/blocks/container.scss';
 
 export const CartPage = () => {
   return (
     <div className="cart">
+      <BackButton />
+      
       <CartItem />
     </div>
   );
