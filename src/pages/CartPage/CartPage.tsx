efee14784e687d9e991e3871ec47f7f915327ae6
Oleksandr Boiko <iccc97@gmail.com>
2024-02-19 12:32:00 +0200

    add styles for `CartPage`

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -3,32 +3,39 @@ import { CartItem } from '../../components/CartItem';
 
 import './cartPage.scss';
 import '../../styles/blocks/button.scss';
+import '../../styles/utils/text-styles.scss';
 
 export const CartPage = () => {
   return (
     <div className="cart">
-      <div className="cart__back-button">
-        <BackButton />
+      <div className="cart__header">
+        <div className="cart__back-button">
+          <BackButton />
+        </div>
+
+        <h1 className="cart__title h1">
+          Cart
+        </h1>
       </div>
 
-      <h1 className="cart__title h1">
-        Cart
-      </h1>
+      <div className="cart__items">
+        <CartItem />
+        <CartItem />
+      </div>
 
       <article className="total">
         <div className="total__info">
-          <div className="total__price">$1337</div>
+          <h2 className="total__price">$1337</h2>
           <div className="total__count-items">Total for 3 items</div>
         </div>
 
         <span className="total__line" />
 
-        <button type="button" className="button">
+        <button type="button" className="button button--add button--cart">
           Checkout
         </button>
       </article>
 
-      <CartItem />
     </div>
   );
 };
