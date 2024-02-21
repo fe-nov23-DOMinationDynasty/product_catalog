8bd7cf8cf602f52e097977d235fa378669660ad3
Vlad Gerula <vlad.gerula@gmail.com>
2024-02-21 23:32:42 +0200

    add mapping categories

--- src/components/ShopCategory/ShopCategory.tsx
+++ src/components/ShopCategory/ShopCategory.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import './shopcategory.scss';
 import '../../styles/utils/text-styles.scss';
+import { Category } from '../../enums/Category';
 
 export const ShopCategory: React.FC = () => {
+  const categories = Object.values(Category);
+
   return (
     <section className="shop" id="shop">
       <h2 className="section-title h2">Shop by category</h2>
       <div className="shop__categories">
-        <article className="product shop__product">
+        {categories.map((category) => (
+          <article className="product shop__product">
+            <div className="product__photo-container">
+              <span className={`product__photo product__photo--${category}`} />
+            </div>
+
+            <h4 className="product__title h4">
+              {category === Category.Phones ? 'Mobile Phones' : category}
+            </h4>
+            <span className="product__counter">95 models</span>
+          </article>
+        ))}
+
+        {/* <article className="product shop__product">
           <div className="product__photo-container">
             <span className="product__photo product__photo--phones" />
           </div>
@@ -30,7 +46,7 @@ export const ShopCategory: React.FC = () => {
           </div>
           <h4 className="product__title h4">Accessories</h4>
           <span className="product__counter">100 models</span>
-        </article>
+        </article> */}
       </div>
     </section>
   );
