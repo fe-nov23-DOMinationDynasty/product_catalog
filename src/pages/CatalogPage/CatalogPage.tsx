daa4528fd03a0ec9afbdd0ddc61fa1c77c6c2ce8
Nikita Krasun <heysleare.ls@gmail.com>
2024-02-21 17:45:23 +0200

    variable in scss crumbs

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Pagination } from '../../components/Pagination';
@@ -17,7 +18,6 @@ export const CatalogPage = () => {
 
   useEffect(() => {
     dispatch(loadProducts(productType as Category));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [productType]);
 
   return (
