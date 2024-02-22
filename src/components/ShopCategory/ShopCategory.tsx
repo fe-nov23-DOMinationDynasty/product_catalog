c5f1d8ef874f31617ed89729fd3985b46a78c45b
Artem <artemmalikov742@gmail.com>
2024-02-22 13:48:53 +0200

    load products on home page appear, show models amount in categories

--- src/components/ShopCategory/ShopCategory.tsx
+++ src/components/ShopCategory/ShopCategory.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './ShopCategory.scss';
 import '../../styles/utils/text-styles.scss';
 import { Category } from '../../enums/Category';
+import { useAppSelector } from '../../app/hooks';
 
 export const ShopCategory: React.FC = () => {
   const categories = Object.values(Category);
+  const { products } = useAppSelector(state => state.productsReducer);
 
   return (
     <section className="shop" id="shop">
@@ -19,34 +21,11 @@ export const ShopCategory: React.FC = () => {
             <h4 className="product__title h4">
               {category === Category.Phones ? 'Mobile Phones' : category}
             </h4>
-            <span className="product__counter">95 models</span>
+            <span className="product__counter">
+              {`${products.filter(product => product.category === category).length} models`}
+            </span>
           </article>
         ))}
-
-        {/* <article className="product shop__product">
-          <div className="product__photo-container">
-            <span className="product__photo product__photo--phones" />
-          </div>
-
-          <h4 className="product__title h4">Mobile phones</h4>
-          <span className="product__counter">95 models</span>
-        </article>
-
-        <article className="product shop__product">
-          <div className="product__photo-container">
-            <span className="product__photo product__photo--tablets" />
-          </div>
-          <h4 className="product__title h4">Tablets</h4>
-          <span className="product__counter">24 models</span>
-        </article>
-
-        <article className="product shop__product">
-          <div className="product__photo-container">
-            <span className="product__photo product__photo--accessories" />
-          </div>
-          <h4 className="product__title h4">Accessories</h4>
-          <span className="product__counter">100 models</span>
-        </article> */}
       </div>
     </section>
   );
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,73 +1,50 @@
-import { Promo } from '../../types/Promo';
+import { useEffect } from 'react';
 import './home-page.scss';
+import '../../styles/utils/text-styles.scss';
 import { PromoSlider } from '../../components/PromoSilder';
 import { tabletWidth } from '../../constants/constants';
 import { useResize } from '../../hooks/useResize';
 import { ShopCategory } from '../../components/ShopCategory';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { actions as productsActions } from '../../features/productsSlice';
+import { promosMobile, promosTabletAndDesktop } from '../../utils/promosHelper';
 
-const promoImagesMobile = [
-  './promos/promo-image-mobile.webp',
-  './promos/banner-phones.png',
-  './promos/banner-tablets.png',
-  './promos/banner-accessories.png',
-];
-const promoImagesTabletAndDesktop = [
-  './promos/promo-image-tablet-desktop.webp',
-  './promos/banner-phones.png',
-  './promos/banner-tablets.png',
-  './promos/banner-accessories.png',
-];
 
-const promosTabletAndDesktop: Promo[] = promoImagesTabletAndDesktop.map(
-  (image, index) => {
-    if (index === 0) {
-      return {
-        image,
-        link: 'https://www.apple.com',
-      };
-    }
-
-    return {
-      image,
-      link: image.replace('promos/banner-', 'catalog/').replace('.png', ''),
-    };
-  }
-);
-
-const promosMobile: Promo[] = promoImagesMobile.map((image, index) => {
-  if (index === 0) {
-    return {
-      image,
-      link: 'https://www.apple.com',
-    };
-  }
-
-  return {
-    image,
-    link: image.replace('promos/banner-', 'catalog/').replace('.png', ''),
-  };
-});
 
 export const HomePage = () => {
   const [windowWidth] = useResize();
 
+  const dispatch = useAppDispatch();
+
+  const { isLoading } = useAppSelector(state => state.productsReducer);
+
+  useEffect(() => {
+    dispatch(productsActions.loadProducts());
+  }, []);
+
   return (
     <section className="home-page">
-      {/* <h1 className="h1">Welcome to Nice Gadgets store!</h1> */}
+      {!isLoading && (
+        <>
+          <h1 className="h1 home-page__title">
+            Welcome to Nice Gadgets store!
+          </h1>
 
-      <PromoSlider
-        promos={
-          windowWidth >= tabletWidth ? promosTabletAndDesktop : promosMobile
-        }
-      />
+          <PromoSlider
+            promos={
+              windowWidth >= tabletWidth ? promosTabletAndDesktop : promosMobile
+            }
+          />
 
-      <ShopCategory />
+          <ShopCategory />
 
-      {/* <CustomSwiper title="Brand new models" items={null} />
+          {/* <CustomSwiper title="Brand new models" items={null} />
 
         <CategoriesFilter />
 
         <CustomSwiper title="Hot prices" items={null} /> */}
+        </>
+      )}
     </section>
   );
 };
