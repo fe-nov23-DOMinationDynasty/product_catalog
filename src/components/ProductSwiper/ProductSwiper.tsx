a56056baadaff549315e565e085f22babf30394d
Artem <artemmalikov742@gmail.com>
2024-02-23 09:18:08 +0200

    implemented mobile swiper vers

--- src/components/ProductSwiper/ProductSwiper.tsx
+++ src/components/ProductSwiper/ProductSwiper.tsx
@@ -1,9 +1,43 @@
-export const ProductSwiper = () => {
+import { SwiperSlide, Swiper } from "swiper/react";
 
+import { Pagination } from 'swiper/modules';
+import React from "react";
+
+import './ProductSwiper.scss';
+
+interface Props {
+  images: string[]
+};
+
+export const ProductSwiper: React.FC<Props> = ({ images }) => {
+  const pagination = {
+    bulletClass: "product-swiper__pagination-item",
+    bulletActiveClass: "product-swiper__pagination-item--active",
+    clickable: true,
+    el: '.product-swiper__pagination',
+    renderBullet: (index: number, className: string) => {
+      return `<div class="${className}"><img src="${images[index]}" alt="pagination_item" class="product-swiper__pagination-image" /></div>`;
+    },
+  };
 
   return (
-    <>
 
-    </>
+    <div className="product-swiper">
+      <Swiper
+        spaceBetween={30}
+        pagination={pagination}
+        modules={[Pagination]}
+        className="product-swiper__swiper">
+        {images.map((image) => (
+          <SwiperSlide key={image} className="product-swiper__slide">
+            <img
+              src={image}
+              alt="product_picture" className="product-swiper__picture"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="product-swiper__pagination" />
+    </div>
   );
 };
--- src/pages/ItemCardPage/ItemCardPage.tsx
+++ src/pages/ItemCardPage/ItemCardPage.tsx
@@ -7,6 +7,7 @@ import { Category } from '../../enums/Category';
 import { Accessory } from '../../types/Accessory';
 import { Tablet } from '../../types/Tablet';
 import { Phone } from '../../types/Phone';
+import { ProductSwiper } from '../../components/ProductSwiper';
 
 export const ItemCardPage = () => {
   const { itemId, productCategory } = useParams();
@@ -18,7 +19,7 @@ export const ItemCardPage = () => {
     getProductInfo(productCategory as Category, itemId as string)
       .then(setCurrentProduct)
       .finally(() => setIsLoading(false));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [itemId]);
 
   return (
@@ -26,9 +27,7 @@ export const ItemCardPage = () => {
       {isLoading && <Loader />}
 
       {!isLoading && (
-        <div>
-          {currentProduct?.capacity || ''}
-        </div>
+        <ProductSwiper images={currentProduct?.images as string[]} />
       )}
     </section>
   );
