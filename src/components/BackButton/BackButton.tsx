7358bf47ca2b91c963a8612e28e817fd8cb11fe8
Artem <artemmalikov742@gmail.com>
2024-02-25 23:50:44 +0200

    fixed back button

--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -1,22 +1,27 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import './backButton.scss';
 import '../../styles/utils/text-styles.scss';
 
 export const BackButton = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const goBack = () => {
+    if (location.key === 'default') {
+      navigate('/');
+
+      return;
+    }
+
     navigate(-1);
   };
 
   return (
-    <>
-      <button type="button" className="back-button small-text" onClick={goBack}>
-        <i className="back-button__arrow-left" />
-        Back
-      </button>
-    </>
+    <button type="button" className="back-button small-text" onClick={goBack}>
+      <i className="back-button__arrow-left" />
+      Back
+    </button>
   );
 };
