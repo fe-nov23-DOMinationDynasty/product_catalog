ff05c1e2f3973b9280028bb7d0370e697144fe5e
Artem <artemmalikov742@gmail.com>
2024-02-26 14:20:14 +0200

    moved static functions out from components

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,39 +6,37 @@ import './footer.scss';
 import '../../styles/blocks/button.scss';
 import '../../styles/utils/text-styles.scss';
 
-export const Footer: React.FC = () => {
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-  };
-
-  return (
-    <footer className="footer">
-      <div className="footer__wrapper container">
-        <LogoLink />
-
-        <div className="footer__links uppercase">
-          <a
-            href="https://github.com/fe-nov23-DOMinationDynasty/product_catalog"
-            className="footer__link"
-            target="_blank"
-            rel="noreferrer">
-            Github
-          </a>
-
-          <Link to="/contacts" className="footer__link">
-            Contacts
-          </Link>
-
-          <Link to="/rights" className="footer__link">
-            Rights
-          </Link>
-        </div>
-
-        <button type="button" className="footer__go-top" onClick={scrollToTop}>
-          <span className="footer__go-top-text">Back to top</span>
-          <i className="button button--top" />
-        </button>
-      </div>
-    </footer>
-  );
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
 };
+
+export const Footer: React.FC = () => (
+  <footer className="footer">
+    <div className="footer__wrapper container">
+      <LogoLink />
+
+      <div className="footer__links uppercase">
+        <a
+          href="https://github.com/fe-nov23-DOMinationDynasty/product_catalog"
+          className="footer__link"
+          target="_blank"
+          rel="noreferrer">
+          Github
+        </a>
+
+        <Link to="/contacts" className="footer__link">
+          Contacts
+        </Link>
+
+        <Link to="/rights" className="footer__link">
+          Rights
+        </Link>
+      </div>
+
+      <button type="button" className="footer__go-top" onClick={scrollToTop}>
+        <span className="footer__go-top-text">Back to top</span>
+        <i className="button button--top" />
+      </button>
+    </div>
+  </footer>
+);
