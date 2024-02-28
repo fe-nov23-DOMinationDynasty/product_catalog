877198b61b69310592731a1db3857a0107f680dd
Oleksandr Boiko <iccc97@gmail.com>
2024-02-28 21:17:14 +0200

    add changes

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,5 +4,7 @@ module.exports = {
     'scss/at-mixin-pattern': null,
     'scss/dollar-variable-empty-line-before': null,
     'scss/at-rule-no-unknown': null,
+    'selector-pseudo-class-no-unknown': null,
+    'property-no-unknown': null,
   },
 };
