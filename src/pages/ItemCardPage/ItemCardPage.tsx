6d67435d5b456dae655b3dcbdbe861f80b05e336
Artem <artemmalikov742@gmail.com>
2024-02-29 04:27:16 +0200

    fixed ProductCard, fixed pagination

--- src/pages/ItemCardPage/ItemCardPage.tsx
+++ src/pages/ItemCardPage/ItemCardPage.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { useEffect, useMemo, useState } from 'react';
+import cn from 'classnames';
 import 'react-loading-skeleton/dist/skeleton.css';
 import Skeleton from 'react-loading-skeleton';
 
